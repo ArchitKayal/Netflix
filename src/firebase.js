c15dad46c48171b37d88c0db389a566e90b0221e
Archit Kayal <architkayal@gmail.com>
2024-02-02 19:37:57 +0530

    created basic navbar

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZq5ONofrzhRAU1lYYbpe3byIAzl-efFE",
+    authDomain: "netflix-clone-d05f5.firebaseapp.com",
+    projectId: "netflix-clone-d05f5",
+    storageBucket: "netflix-clone-d05f5.appspot.com",
+    messagingSenderId: "873143549039",
+    appId: "1:873143549039:web:a90bcf8169d473090d219a"
+  };
