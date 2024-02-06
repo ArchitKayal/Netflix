3d735b9900de3de52b5343f7db694e329c516e30
Archit Kayal <architkayal@gmail.com>
2024-02-06 23:55:37 +0530

    created basic login logout functionality

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCZq5ONofrzhRAU1lYYbpe3byIAzl-efFE",
     authDomain: "netflix-clone-d05f5.firebaseapp.com",
@@ -6,3 +10,9 @@ const firebaseConfig = {
     messagingSenderId: "873143549039",
     appId: "1:873143549039:web:a90bcf8169d473090d219a"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
